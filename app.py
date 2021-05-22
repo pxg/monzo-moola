4613b730870ac8e334a66f7ee8c7d4b803a2195a
Pete Graham <pete.graham@zerodeposit.com>
2021-05-22 12:20:36 +0100

    Format code with Black

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ def get_balance_api_url(account_id):
     """
     Get Balance URL for Monzo API
     """
-    return 'https://api.getmondo.co.uk/balance?account_id={}'.format(
-        account_id)
+    return "https://api.getmondo.co.uk/balance?account_id={}".format(account_id)
 
 
 def get_current_date():
@@ -23,17 +22,19 @@ def get_current_balance(account_id, access_token):
     """
     response = requests.get(
         get_balance_api_url(account_id),
-        headers={'Authorization': 'Bearer {}'.format(access_token)})
-    return response.json()['balance']
+        headers={"Authorization": "Bearer {}".format(access_token)},
+    )
+    return response.json()["balance"]
 
 
 def write_to_google_sheets(balance):
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     balance = get_current_balance(
-        account_id=os.environ.get('MONZO_ACCOUNT_ID'),
-        access_token=os.environ.get('MONZO_ACCESS_TOKEN'))
-    print('balance in pence {}'.format(balance))
+        account_id=os.environ.get("MONZO_ACCOUNT_ID"),
+        access_token=os.environ.get("MONZO_ACCESS_TOKEN"),
+    )
+    print("balance in pence {}".format(balance))
     write_to_google_sheets(balance)
--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ from freezegun import freeze_time
 from app import get_current_date
 
 
-@freeze_time('1983-07-06')
+@freeze_time("1983-07-06")
 def test_get_current_date():
     year, month, day = get_current_date()
 
